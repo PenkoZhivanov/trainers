15d82f891b42acc943b50a84e66400aadd4b96c3
Penko Zhivanov <penko_@hotmail.com>
2020-01-11 20:07:57 +0200

    gg

--- classes/Form.php
+++ classes/Form.php
@@ -11,48 +11,64 @@ class Form {
      * field action (for buttons)
      * 
      */
-    var $data=null;
-    
-    public function createForm(){
+
+    var $data = null;
+    var $name, $id, $type, $value, $min, $max, $text, $required, $action;
+
+    public function createForm() {
         $form = "";
-        $spc=" ";
+        $spc = " ";
         foreach ($this->data as $element) {
-            $type=$element['type'];
-            $id=$element['id']!=null?'id="'.$element['id'].'"':"";
-            $name = $element['name']!=null?'name="'.$element['name'].'"':"";;
-            $value = $element['value']!=null?'value="'.$element['value'].'"':"";
-            $min= $element['min']!=null?'min="'.$element['min'].'"':"";
-            $max =  $element['max']!=null?'max="'.$element['max'].'"':"";
-            $text=$element['text']!=null?$element['text']:"";
-            $required = $element['required']!=null?'required="'.$element['required'].'"':"";
-            $action = $element['action']!=null?'action="'.$element['action'].'"':"";
-            $form = $form."<".$type.$spc.$id.$spc.$name.$spc.$value.$spc.$min.$spc.$max.$spc.">".$text;
-            if($element["type"]=="textarea"){
-                $form = $form."</textarea>";
-            }
-               if($element["type"]=="select"){
-                $form = $form."<option value='0'>Изберете</option></select>";
-            }
+            $type = $element['type'];
+            $id = $element['id'] != null ? 'id="' . $element['id'] . '"' : "";
+            $name = $element['name'] != null ? 'name="' . $element['name'] . '"' : "";
             
-            
-            if($element['type']=="label"){
-                $form="<p>".$form."</label>&nbsp;&nbsp;";
-            }else{
-                $form=$form."</p>";
+            $value = $element['value'] != null ? 'value="' . $element['value'] . '"' : "";
+            $min = $element['min'] != null ? 'min="' . $element['min'] . '"' : "";
+            $max = $element['max'] != null ? 'max="' . $element['max'] . '"' : "";
+            $text = $element['text'] != null ? $element['text'] : "";
+            $required = $element['required'] != null ? 'required="' . $element['required'] . '"' : "";
+            $action = $element['action'] != null ? 'action="' . $element['action'] . '"' : "";
+            $method = $element['method'] != null ? 'method="' . $element['method'] . '"' : "";
+
+            $form = $form . "<" . $type . $spc . $id . $spc . $name . $spc . $value . $spc . $min . $spc . $max . $spc . $action.$spc.$method.">" . $text;
+            if ($element["type"] == "textarea") {
+                $form = $form . "</textarea>";
             }
-            
-          
+            if ($element["type"] == "select") {
+                $form = $form . "<option value='0'>Изберете</option></select>";
+            }
+
+
+            if ($element['type'] == "label") {
+                $form = "<p>" . $form . "</label>&nbsp;&nbsp;";
+            } else {
+                $form = $form . "</p>";
+            }
         }
         echo $form;
+
+    }
+    
+
+    private function filert($data){
+        
+        if(!isset($data)||$data==null){
+            return "";
+        }
+        print_r(array_keys($this->data[0],$data));
+        print_r($this->data);
+        return array_keys($this->data[0],$data);
     }
     
     public function addElement($data) {
         $this->data[] = $data;
     }
-    
+
     public function print() {
         echo "<pre>";
         print_r($this->data);
         echo "</pre>";
     }
+
 }
